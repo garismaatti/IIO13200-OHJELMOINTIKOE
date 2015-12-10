207b6630bc0c1226eb30f69359ad36a63cfc2c87
garismaatti <ari.salopaa@gmail.com>
2015-12-10 13:29:04 +0200

    Tehtävä T1b valmis ja toimii

---
+++ Tehtavat/K2362_T1b/Laskuri.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2362_T1b
+{
+    class Laskuri
+    {
+        private List<char> allowd = new List<char>();
+
+        public int stringLength(string str_source)
+        {
+            return str_source.Length;
+        }
+
+        public List<string> calculateString(string str_source)
+        {
+            //Update allowd char
+            string a = "abcdefghijklmnopqrstuvwyxzåäö";
+            char[] b = a.ToCharArray();
+            allowd.Clear();
+            allowd.AddRange(b);
+
+            //make lower case
+            string sourceStr = str_source.ToLower();
+            //init needed list
+            List<char> Astr = sourceStr.ToList();
+            List<char> parsedList = new List<char>();
+            List<int> parsedListCount = new List<int>();
+            List<string> finalList = new List<string>();
+
+            for (int i = 0; i < Astr.Count; i++){
+
+
+                //If char is in allowd list
+                if (allowd.Contains(Astr[i]))  
+                {
+                    if (parsedList.Contains(Astr[i]))
+                    {
+                        //If in list
+
+                        //find index number and add +1 to counter
+                        for (int k = 0; k < parsedList.Count; k++)
+                        {
+                            if (parsedList.ElementAt(k) == Astr[i])
+                            {
+                                int tmpNum = parsedListCount[k];
+                                tmpNum++;
+                                parsedListCount[k] = tmpNum;
+                            }
+                        }
+
+
+                    }
+                    else
+                    {
+                        //if not in list
+                        parsedList.Add(Astr[i]);
+                        parsedListCount.Add(1);
+
+                    }
+                }
+
+            }
+
+            //Create a row to but in result
+            for (int l = 0; l < parsedList.Count; l++)
+            {
+                string tmpStr = "";
+                tmpStr += parsedList[l];
+                tmpStr += " = ";
+                tmpStr += parsedListCount[l];
+                finalList.Add(tmpStr);
+            }
+
+            finalList.Sort();   //Sort final list
+            return finalList;
+
+        }
+    }
+}
---
+++ Tehtavat/K2362_T1b/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace K2362_T1b
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //init class
+        private Laskuri lsk = new Laskuri();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Calculate button
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> list = lsk.calculateString(tbText.Text);
+            lbResult.Items.Clear();
+            for (int i = 0; i < list.Count; i++)
+            {
+                lbResult.Items.Add(list[i]);
+            }
+
+            string sum = "Yhteensä ";
+            sum += tbText.Text.Length;
+            sum += " ja ";
+            sum += list.Count;
+            sum += " eri kirjainta.";
+            labResul.Content = sum;
+        }
+
+        //Clear input
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbText.Clear();
+        }
+
+        //Creal all
+        private void btnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            tbText.Clear();
+            lbResult.Items.Clear();
+            labResul.Content = "Yhteensä 0 merkkiä ja 0 eri kirjainta.";
+        }
+    }
+}
